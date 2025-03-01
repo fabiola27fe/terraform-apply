617ad80260f0b5226394977483948b192935157d
Fabiola Fernandez Muñoz <fabiolafernandezmunoz@MacBook-Air-de-Fabiola.local>
2025-03-01 17:52:29 +0100

    update code

--- main.tf
+++ main.tf
@@ -1,37 +1,13 @@
-module "this" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.5.0"
-
-  name          = var.hostname
-  instance_type = local.size[var.size].instance_type
-
-  ami                = data.aws_ami.os.id
-  ignore_ami_changes = true
-
-  create_iam_instance_profile = false
-  iam_instance_profile        = "mapfre-talos-instance-profile"
-
-  enable_volume_tags = true
-  root_block_device = [
-    {
-      encrypted   = true
-      volume_type = "gp3"
-      volume_size = var.disk_size
-    }
-  ]
-
-  subnet_id                   = data.aws_subnet.subnet.id
-  associate_public_ip_address = false
-  ipv6_address_count          = 0
-
-  user_data = local.os[var.os].user_data
-
-  tags = var.tags
+provider "aws" {
+  region = var.region
 }
 
-data "aws_subnet" "subnet" {
-  filter {
-    name   = "tag:Name"
-    values = [var.subnet_name]
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name        = "EC2-${var.id}"
+    Environment = var.env
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,65 +1,24 @@
-variable "os" {
+variable "region" {
+  description = "AWS region where the EC2 instance will be deployed"
   type        = string
-  description = "Sistema operativo para desplegar"
 }
 
-variable "hostname" {
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
   type        = string
-  description = "Nombre de la instancia"
 }
 
-variable "hostname_prefix" {
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
   type        = string
-  description = "Prefijo del nombre de instancia - ignorado por Terraform"
-  default     = ""
 }
 
-variable "disk_size" {
-  type        = number
-  description = "Tamaño de disco en GB"
-  default     = 50
-
-  validation {
-    condition     = var.disk_size == floor(var.disk_size)
-    error_message = "El tamaño de disco debe ser entero"
-  }
-
-  validation {
-    condition     = var.disk_size >= 50
-    error_message = "El tamaño de disco debe ser mayor o igual a 50"
-  }
-}
-
-variable "size" {
+variable "id" {
+  description = "Unique identifier for the EC2 instance"
   type        = string
-  description = "Tamaño de instancia, 'S', 'M' o 'L'"
-  default     = "S"
-
-  validation {
-    condition     = contains(["S", "M", "L"], var.size)
-    error_message = "El tamaño de instancia debe ser uno de S/M/L"
-  }
 }
 
-variable "subnet_name" {
+variable "env" {
+  description = "Environment where the EC2 instance will be deployed"
   type        = string
-  description = "Nombre de la subnet"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "Etiquetas adicionales"
-  default     = {}
-}
-
-variable "talos_unique_id" {
-  type        = string
-  description = "Identificador único de Talos, suministrado por el workflow"
-  default     = ""
-}
-
-variable "ssm_parameter_name" {
-  type        = string
-  description = "Nombre de parámetro SSM en que almacenar identificador de Instancia"
-  default     = ""
 }
